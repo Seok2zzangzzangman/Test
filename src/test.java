a05f7e8412cb906fde63207e1c5a35184362d4b1
seok2zzangzzangman <seok2zzangzzangman@localhost>
2022-03-21 16:33:16 +0900

    복

--- src/test.java
+++ src/test.java
@@ -1,4 +1,16 @@
-
+import java.util.Scanner;
 public class test {
-
+public static void main (String[]args) {
+	System.out.println("출력할 구구단 단수는?");	
+	Scanner scanner = new Scanner (System.in);
+	int number = scanner.nextInt(); 
+	System.out.println("사용자가 입력한 값은 : " + number);
+	if (number<2)  { System.out.println("2~9사이 숫자를 입력하시오");
+	}else if  (number>9) { System.out.println("2~9사이 숫자를 입력하시오");
+	}
+	else for (int i = 1 ; i <10 ; i++ ) {
+		System.out.println(number*i);
+		
+	}
+	}
 }
