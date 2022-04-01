aaf7235f8e7b69be4efd206d2bfce8be37e673c7
seok2zzangzzangman <seok2zzangzzangman@localhost>
2022-04-01 15:11:30 +0900

    헤헤처음으로 문제 풀었다 !

--- src/test.java
+++ src/test.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
-public class test {
-public static void main (String[]args) {
-	System.out.println("출력할 구구단 단수는?");	
-	Scanner scanner = new Scanner (System.in);
-	int number = scanner.nextInt(); 
-	System.out.println("사용자가 입력한 값은 : " + number);
-	if (number<2)  { System.out.println("2~9사이 숫자를 입력하시오");
-	}else if  (number>9) { System.out.println("2~9사이 숫자를 입력하시오");
-	}
-	else for (int i = 1 ; i <10 ; i++ ) {
-		System.out.println(number*i);
-		
-	}
-	}
+public class test{
+public static void main(String[]args) {
+Scanner scanner = new Scanner(System.in);
+ int number = scanner.nextInt();
+ System.out.println("구현할 단수는? : " + number );
+ for ( int i = 0 ; i <20 ; i ++){
+ if ( i<number ) {
+	 
+	 System.out.println(number*(i+1));
+ }
+
+ }
+ }
+
+	 
+	 
 }
