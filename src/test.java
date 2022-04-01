fd9dec0f7cd367bba78af720e1ece891031867b4
seok2zzangzzangman <seok2zzangzzangman@localhost>
2022-04-01 15:12:24 +0900

    ㅎㅎㅋㅎㅋㅎㅋ처음으로풀어씅

--- src/test.java
+++ src/test.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
+import java.util.Scanner; // 입력값 받아오기	
 public class test{
 public static void main(String[]args) {
-Scanner scanner = new Scanner(System.in);
- int number = scanner.nextInt();
+Scanner scanner = new Scanner(System.in); //입력값 받아오기 
+ int number = scanner.nextInt(); // 입력값 형식? 정해주
  System.out.println("구현할 단수는? : " + number );
  for ( int i = 0 ; i <20 ; i ++){
  if ( i<number ) {
